696dcc07c001f78a1294fc35e6a0b1e280c85188
Nathan Snyder <nathan@snydern.com>
2014-10-13 11:25:56 -0400

    Sound files

--- constants.h
+++ constants.h
@@ -52,6 +52,13 @@ const UCHAR ESC_KEY      = VK_ESCAPE;   // escape key
 const UCHAR ALT_KEY      = VK_MENU;     // Alt key
 const UCHAR ENTER_KEY    = VK_RETURN;   // Enter key
 
+// audio files required by audio.cpp
+// WAVE_BANK must be location of .xwb file.
+const char WAVE_BANK[]  = "audio\\Win\\pong.xwb";
+// SOUND_BANK must be location of .xsb file.
+const char SOUND_BANK[] = "audio\\Win\\pong.xsb";
 
+// audio cues
+const char HIT[] = "hit";
 
 #endif
